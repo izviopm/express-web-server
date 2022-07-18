a50d8ea73fd7d585e8ef8b14b4bfc454deb40d44
Izvio Prijaya Mumin <prijayaizvio@gmail.com>
2022-07-18 15:15:15 +0700

    First Commit

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const { loadContact } = require('../utils/contacts');
+
+const app = express();
+const port = 3000;
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts); // third party middleware
+app.use(express.static('public')); // built-in middleware
+
+app.get('/', (req, res) => {
+  const mahasiswa = [
+    {
+      nama: 'Izvio Prijaya Mumin',
+      email: 'izvioprijaya@gmail.com',
+    },
+    {
+      nama: 'Alif Muhammad Nuha',
+      email: 'alif748@gmail.com',
+    },
+    {
+      nama: 'Egan Kusmaya Putra',
+      email: 'egn234@gmail.com',
+    },
+  ];
+  res.render('index', {
+    layout: 'layouts/main-layout',
+    nama: 'Izvio Prijaya Mumin',
+    title: 'Halaman Home',
+    mahasiswa,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman About',
+  });
+});
+
+app.get('/contact', (req, res) => {
+  const contacts = loadContact();
+  res.render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman Contact',
+    contacts,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
